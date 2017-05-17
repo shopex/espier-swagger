db9d775d8568978bb95da362a87d13feffca24c2
wuwei <wuwei@shopex.cn>
2017-05-17 14:45:20 +0800

    新增asset支持

---
+++ src/heplers.php
@@ -0,0 +1,23 @@
+<?php
+
+if (!function_exists('urlGenerator')) {
+    /**
+     * @return \Laravel\Lumen\Routing\UrlGenerator
+     */
+    function urlGenerator() {
+        return new \Laravel\Lumen\Routing\UrlGenerator(app());
+    }
+}
+
+if (!function_exists('asset')) {
+    /**
+     * @param $path
+     * @param bool $secured
+     *
+     * @return string
+     */
+    function asset($path, $secured = false) {
+        return urlGenerator()->asset($path, $secured);
+    }
+}
+
